ecc64bebb3fd7694960d546ed025549d104e0eec
Jonathan Rainer <jonrainer@googlemail.com>
2013-07-06 08:13:45 +0100

    Begun work on the Control Office entry form GUI.

---
+++ src/gui/ControlOfficeEntryForm.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jonathanrainer
+ */
+public class ControlOfficeEntryForm {
+
+    // A frame to hold the content of this entry form.
+    private JFrame mainFrame;
+    // The template to give a templated JFrame within which the content can 
+    // be placed.
+    private Template template;
+
+    public ControlOfficeEntryForm() {
+        
+        
+    }
+}
