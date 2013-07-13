864d07be93ef478f4a42a3417e4d0b40a8742b84
Jonathan Rainer <jonrainer@googlemail.com>
2013-07-14 00:06:39 +0100

    Added in more data to be loaded into Nexus

--- src/sql/data.sql
+++ src/sql/data.sql
@@ -3,14 +3,32 @@ TRUNCATE TABLE `Nexus`.`Users`;
 INSERT INTO `Nexus`.`Users`
 (`name`,`team`, `password`)
 VALUES
-('Helene Roe','Control Office', NULL),
+/* Admins and Super Users */
+('Ian Walker', 'Super Users', NULL),
+('Jonathan Rainer', 'Super Users', NULL),
+('Andrew Wyss', 'Super Users', NULL),
+('CT SU1', 'Super Users', NULL),
+('CT SU2', 'Super Users', NULL),
+('Info SU1', 'Super Users', NULL),
+('Info SU2', 'Super Users', NULL),
+('Admin SU1', 'Super Users', NULL),
+('Admin SU2', 'Super Users', NULL),
+/* Control Office */
 ('Andrew Wyss','Control Office', NULL),
 ('Katie Tupling','Control Office', NULL),
 ('Ian Walker','Control Office', NULL),
-('John','Control Office', NULL),
-('Mel Meesam','Control Office', NULL),
-('Colin','Control Office', NULL),
+('Alison Fogg','Control Office', NULL),
+('Colin Evans','Control Office', NULL),
 ('Hannah Allison','Control Office', NULL),
+('Helene Roe','Control Office', NULL),
+('John Haslam','Control Office', NULL),
+('Mel Meesam','Control Office', NULL),
+('Peter Frymann','Control Office', NULL),
+('Richard Lawrence','Control Office', NULL),
+('Wayne [Night Crew]','Control Office', NULL),
+/* Information Team */
+('Mark Rance', 'Information Team', NULL),
 /* Admin Team */
 ('David Jones','Administration Team', NULL),
-('Jonathan Rainer','Administration Team', NULL);
+('Jonathan Rainer','Administration Team', NULL)
+;
