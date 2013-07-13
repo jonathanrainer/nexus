4c0b8ff1543916fd066acb2f8e2e2d04f2096d9d
Jonathan Rainer <jonrainer@googlemail.com>
2013-07-13 08:22:11 +0100

    Made changes to reflect the changes in thought on how the database should work. Still a work in progress.

--- src/sql/databaseSchema.sql
+++ src/sql/databaseSchema.sql
@@ -3,23 +3,23 @@ CREATE DATABASE `Nexus`;
 USE `Nexus`;
 
 CREATE TABLE `Tickets` (
-`id` INT(4) NOT NULL,
+`id` INT(4) NOT NULL AUTO_INCREMENT,
 `dateTime` DATETIME DEFAULT 0 NOT NULL,
-`createdBy` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-`problemLocation` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-`problemDescription` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-`CISKeywords` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+`createdBy` VARCHAR(255) NOT NULL,
+`problemLocation` VARCHAR(255) NOT NULL,
+`problemDescription` VARCHAR(255) NOT NULL,
+`CISKeywords` VARCHAR(255) NOT NULL,
 `delegateImpact` ENUM('Low', 'Medium', 'High') NOT NULL,
 PRIMARY KEY (`id`)
-) Engine = InnoDB;
+) Engine = InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE TABLE `Users` (
-`userID` CHAR(6) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-`name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+`userID` INT(4) AUTO_INCREMENT  NOT NULL,
+`name` VARCHAR(255) NOT NULL,
 `team` ENUM('Super Users', 'Control Office', 'Information Team', 'Administration Team') NOT NULL,
-`password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+`password` VARCHAR(255) NULL,
 PRIMARY KEY (`userID`)
-) Engine = InnoDB;
+) Engine = InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
 
 
 --CREATE TABLE `Verification-Queue` (
