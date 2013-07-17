fb0712263c0250eb38d9f8f5f5419c8b7565ed84
Jonathan Rainer <jonrainer@googlemail.com>
2013-07-17 08:01:58 +0100

    Begun to refactor form design to allow it to be updated more easily. Needs work in form class and adding functionality class.

--- src/system/MainSystem.java
+++ src/system/MainSystem.java
@@ -200,7 +200,285 @@ public class MainSystem
      */
     private ControlOfficeEntryForm createLimitedEntryForm()
     {
-        final ControlOfficeEntryForm cofe = new ControlOfficeEntryForm(true);
+        final ControlOfficeEntryForm cofe = new ControlOfficeEntryForm(true, user);
+        //Set the text in the two automated fields and make them unwriteable
+        cofe.getTicketReferenceTextField().setText("Automated");
+        cofe.getTicketReferenceTextField().setEditable(false);
+        cofe.getDateTimeTextField().setText("Automated");
+        cofe.getDateTimeTextField().setEditable(false);
+
+        //Add to the ticket written combo box with the team the user is from
+        cofe.getTicketWrittenComboBox().addItem(user.getTeam());
+        cofe.getTicketWrittenComboBox().setSelectedIndex(0);
+        cofe.getTicketWrittenComboBox().setEnabled(false);
+
+        //Iterate over the team members to give a choice
+        ArrayList<String> teamMembers = mysqlEngine.
+                enumerateTeamMembers(user.getTeam());
+        cofe.getTeamMembersComboBox().addItem("Choose One");
+        Iterator<String> it1 = teamMembers.iterator();
+        while (it1.hasNext())
+        {
+            cofe.getTeamMembersComboBox().addItem(it1.next());
+        }
+
+        Iterator it2 = dataStructures.getMasterListBox1().iterator();
+        cofe.getProblemLocationComboBox1().addItem("Select a Location");
+        while (it2.hasNext())
+        {
+            cofe.getProblemLocationComboBox1().addItem(it2.next());
+        }
+        //Create HashMap for ComboBox 2 it should link together the string
+        //selected in the previous box with the new set of strings for the next
+        //combo box.
+
+
+        cofe.getProblemLocationComboBox1().addActionListener(
+                new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                if (e.getActionCommand().equals("comboBoxChanged")
+                        && !(cofe.getProblemLocationComboBox1().getSelectedItem().
+                        equals("Select a Location")))
+                {
+                    ArrayList<String> secondBoxOptions = dataStructures.getMasterListBox2().
+                            get(cofe.getProblemLocationComboBox1().getSelectedItem().toString());
+                    cofe.getProblemLocationComboBox2().removeAllItems();
+                    cofe.getProblemLocationComboBox3().removeAllItems();
+                    cofe.getProblemLocationComboBox4().removeAllItems();
+                    Iterator<String> secondBoxIterator = secondBoxOptions.iterator();
+                    cofe.getProblemLocationComboBox2().addItem("Select a Location");
+                    while (secondBoxIterator.hasNext())
+                    {
+                        cofe.getProblemLocationComboBox2().addItem(secondBoxIterator.next());
+                    }
+                    cofe.getProblemLocationComboBox2().setSelectedIndex(0);
+                }
+                else if(cofe.getProblemLocationComboBox1().getSelectedItem().
+                        equals("Select a Location"))
+                {
+                    cofe.getProblemLocationComboBox2().removeAllItems();
+                    cofe.getProblemLocationComboBox3().removeAllItems();
+                    cofe.getProblemLocationComboBox4().removeAllItems();
+                }
+            }
+        });
+
+        cofe.getProblemLocationComboBox2().addActionListener(
+                new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                if (e.getActionCommand().equals("comboBoxChanged") 
+                        && !(cofe.getProblemLocationComboBox2().getSelectedItem() == null)
+                        && !(cofe.getProblemLocationComboBox2().getSelectedItem().equals("Select a Location")))
+                    {
+                        ArrayList<String> thirdBoxOptions = dataStructures.getMasterListBox3().get(
+                                cofe.getProblemLocationComboBox2().getSelectedItem().toString());
+                        cofe.getProblemLocationComboBox3().removeAllItems();
+                        cofe.getProblemLocationComboBox4().removeAllItems();
+                        Iterator<String> thirdBoxIterator = thirdBoxOptions.iterator();
+                        if (!(thirdBoxOptions.size() == 1))
+                        {
+                            cofe.getProblemLocationComboBox3().addItem("Select a Location");
+                        }
+                        while (thirdBoxIterator.hasNext())
+                        {
+                            cofe.getProblemLocationComboBox3().addItem(thirdBoxIterator.next());
+                        }
+
+                    }
+                if (!(cofe.getProblemLocationComboBox2().getSelectedItem() == null)
+                        && (cofe.getProblemLocationComboBox2().getSelectedItem().equals("Select a Location")))
+                {
+                    cofe.getProblemLocationComboBox3().removeAllItems();
+                    cofe.getProblemLocationComboBox4().removeAllItems();
+                }
+            }
+        });
+
+        cofe.getProblemLocationComboBox3().addActionListener(
+                new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                if (e.getActionCommand().equals("comboBoxChanged") 
+                        && !(cofe.getProblemLocationComboBox3().getSelectedItem() == null 
+                        || cofe.getProblemLocationComboBox3().getSelectedItem().equals("Select a Location")))
+                    {
+                        ArrayList<String> fourthBoxOptions;
+                        if (cofe.getProblemLocationComboBox3().getSelectedItem().equals("NA"))
+                        {
+                            fourthBoxOptions =
+                                    dataStructures.getMasterListBox4().get(cofe.
+                                    getProblemLocationComboBox2().
+                                    getSelectedItem() + "-"
+                                    + cofe.getProblemLocationComboBox3().
+                                    getSelectedItem());
+                        } else
+                        {
+                            fourthBoxOptions =
+                                    dataStructures.getMasterListBox4().get(cofe.getProblemLocationComboBox3()
+                                    .getSelectedItem().toString());
+                        }
+                        cofe.getProblemLocationComboBox4().removeAllItems();
+                        Iterator<String> fourthBoxIterator =
+                                fourthBoxOptions.iterator();
+                        if (!(fourthBoxOptions.size() == 1))
+                        {
+                            cofe.getProblemLocationComboBox4().addItem("Select a Facility");
+                        }
+                        while (fourthBoxIterator.hasNext())
+                        {
+                            cofe.getProblemLocationComboBox4().addItem(fourthBoxIterator.next());
+                        }
+                    }
+                if (!(cofe.getProblemLocationComboBox3().getSelectedItem() == null)
+                        && (cofe.getProblemLocationComboBox3().getSelectedItem().equals("Select a Location")))
+                {
+                    cofe.getProblemLocationComboBox4().removeAllItems();
+                }
+
+                }
+            });
+
+        cofe.getWhoIsAComboBox().addItem("Choose One");
+        cofe.getWhoIsAComboBox().addItem("Delegate");
+        cofe.getWhoIsAComboBox().addItem("Speaker");
+        cofe.getWhoIsAComboBox().addItem("Team Member");
+        
+        cofe.getContactViaComboBox().addItem("Choose One");
+        cofe.getContactViaComboBox().addItem("Mobile");
+        cofe.getContactViaComboBox().addItem("Radio");
+        cofe.getContactViaComboBox().addItem("Village Host");
+        cofe.getContactViaComboBox().addItem("Village");
+        cofe.getContactViaComboBox().addItem("Not Required");
+        
+        cofe.getContactViaComboBox().addActionListener(new ActionListener()
+                {
+                   @Override
+                   public void actionPerformed(ActionEvent e)
+                   {
+                       if(e.getActionCommand().equals("comboBoxChanged") &&
+                               !(cofe.getContactViaComboBox().getSelectedItem().
+                               toString().equals("Choose One")) && 
+                               !(cofe.getContactViaComboBox().getSelectedItem().
+                               toString().equals("Mobile")))
+                       {
+                           cofe.getContactNumberTextField().setText(
+                                   cofe.getContactViaComboBox().
+                                   getSelectedItem().toString());
+                       }
+                       else
+                       {
+                           cofe.getContactNumberTextField().setText("");
+                       }
+                   }
+                });
+        
+        cofe.getLocationVenueVillageComboBox().addItem("Choose One");
+        cofe.getLocationVenueVillageComboBox().addItem("Venue");
+        cofe.getLocationVenueVillageComboBox().addItem("Village");
+        cofe.getLocationVenueVillageComboBox().addItem("Not Required");
+ 
+        cofe.getSubmitFormButton().addActionListener(
+                new ActionListener()
+                {
+                    @Override
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        String team = user.getTeam();
+                        user.setName(cofe.getTeamMembersComboBox().getSelectedItem().toString());
+                        String member = user.getName();
+                        String problemLocation = cofe.
+                                getProblemLocationComboBox1().getSelectedItem()
+                                + "-" + cofe.getProblemLocationComboBox2().getSelectedItem()
+                                + "-" + cofe.getProblemLocationComboBox3().getSelectedItem()
+                                + "-" + cofe.getProblemLocationComboBox4().getSelectedItem();
+                        String problemDescription = cofe.getProblemDescriptionTextArea().getText();
+                        Iterator<JRadioButton> buttonsIterator = cofe.getButtonsInGrid().iterator();
+                        ArrayList<String> keyWords = new ArrayList<String>();
+                        while (buttonsIterator.hasNext())
+                        {
+                            JRadioButton buttonUnderConsideration = buttonsIterator.next();
+                            if(buttonUnderConsideration.isSelected())
+                            {
+                                keyWords.add(buttonUnderConsideration.getName());
+                            }
+                        }
+                        String problemReportedBy = cofe.getProblemReportedByTextField().getText();
+                        String whoIsA = cofe.getWhoIsAComboBox().getSelectedItem().toString();
+                        String contactVia = cofe.getContactViaComboBox().getSelectedItem().toString();
+                        String contactNumber = cofe.getContactNumberTextField().getText();
+                        String locationVenueVillage = (String) cofe.getLocationVenueVillageComboBox().
+                                getSelectedItem().toString();
+                        Ticket ticket = new Ticket(0, null, user, 
+                                problemLocation, problemDescription, keyWords, 
+                                problemReportedBy, whoIsA, contactVia, contactNumber,
+                                locationVenueVillage, "Low", false, null, "Issue Reported", null, null,
+                                null, null, null);
+                        String validationResult = ticket.dataValidationEntry();
+                        if(validationResult.equals("Passed"))
+                        {
+                            if(!(mysqlEngine.isTicketDuplicate(ticket)))
+                            {
+                                if(mysqlEngine.submitTicket(ticket, false))
+                                {
+                                    JOptionPane.showMessageDialog(cofe.getMainFrame(),
+                                            "The Job Ticket has been submitted. \n"
+                                            + "The Job ID is: " + ticket.getJobRefId()
+                                            + "\nIt was submitted at: " + ticket.getDateTime().toString("d/M/y H:m"));
+                                    cofe.getMainFrame().dispose();
+                                }
+                                else
+                                {
+                                    JOptionPane.showMessageDialog(cofe.getMainFrame(), "MYSQL isn't happy...");
+                                }
+                            }
+                            else
+                            {
+                                mysqlEngine.submitTicket(ticket, true);
+                            }
+                        }   
+                        else
+                        {
+                            JOptionPane.showMessageDialog(cofe.getMainFrame(), validationResult);
+                        }
+                    }
+                });
+        
+        cofe.getResetFormButton().addActionListener(
+                new ActionListener()
+                {
+                    @Override
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        cofe.getTicketWrittenComboBox().setSelectedIndex(0);
+                        cofe.getProblemLocationComboBox1().setSelectedIndex(0);
+                        cofe.getProblemDescriptionTextArea().setText("");
+                        Iterator<JRadioButton> buttonsIterator = cofe.getButtonsInGrid().iterator();
+                        while(buttonsIterator.hasNext())
+                        {
+                            buttonsIterator.next().setSelected(false);
+                        }
+                        cofe.getProblemReportedByTextField().setText("");
+                        cofe.getWhoIsAComboBox().setSelectedIndex(0);
+                        cofe.getContactViaComboBox().setSelectedIndex(0);
+                        cofe.getContactNumberTextField().setText("");
+                        cofe.getLocationVenueVillageComboBox().setSelectedIndex(0);
+                    }
+                });
+        
+        return cofe;
+    }
+
+    private ControlOfficeEntryForm createControlEntryForm()
+    {
+        final ControlOfficeEntryForm cofe = new ControlOfficeEntryForm(true, user);
         //Set the text in the two automated fields and make them unwriteable
         cofe.getTicketReferenceTextField().setText("Automated");
         cofe.getTicketReferenceTextField().setEditable(false);
@@ -383,7 +661,7 @@ public class MainSystem
         cofe.getLocationVenueVillageComboBox().addItem("Venue");
         cofe.getLocationVenueVillageComboBox().addItem("Village");
         cofe.getLocationVenueVillageComboBox().addItem("Not Required");
- 
+        
         cofe.getSubmitFormButton().addActionListener(
                 new ActionListener()
                 {
@@ -474,7 +752,7 @@ public class MainSystem
         
         return cofe;
     }
-
+    
     private ControlOfficeEntryForm createUpdateAmendEntryForm()
     {
         String ticketID = JOptionPane.showInputDialog(
@@ -482,7 +760,7 @@ public class MainSystem
                 "Please enter the Ticket ID of the Job Ticket you wish to"
                 + "update.", "Update/Amend Ticket Search", 
                 JOptionPane.QUESTION_MESSAGE);
-        final ControlOfficeEntryForm cofeAmmend = new ControlOfficeEntryForm(false);
+        final ControlOfficeEntryForm cofeAmmend = new ControlOfficeEntryForm(false, user);
         Ticket ticket = mysqlEngine.retrieveTicket(ticketID);
         
         //Add information back into the ticket
