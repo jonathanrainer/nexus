23bfdc52cabce8ae6de2362c6eec6da7c88990ce
Jonathan Rainer <jonrainer@googlemail.com>
2013-07-25 23:06:17 +0100

    Removed code that was surplus to requirements so the dashboard can be worked on properly.

--- src/gui/DashBoard.java
+++ src/gui/DashBoard.java
@@ -5,17 +5,6 @@
 package gui;
 
 import io.MYSQLEngine;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.util.ArrayList;
-import java.util.Iterator;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import system.DataCollector;
-import system.Ticket;
 import gui.Template;
 
 /**
@@ -24,95 +13,18 @@ import gui.Template;
  */
 public final class DashBoard {
     
-    private JFrame mainFrame;
-    private DataCollector dataCollector;
-    private GridBagLayout layout;
     private Template template;
-    private ArrayList<Ticket> issueReportedTickets;
-    private ArrayList<Ticket> ticketPrintedTickets;
-    private ArrayList<Ticket> jobInProgressTickets;
-    private ArrayList<Ticket> newTickets;
-    private static final int TOPROW = 0;
+    
     
     public DashBoard(MYSQLEngine mysqlEngine)
     {
         template = new Template();
+       
         
-        issueReportedTickets = new ArrayList<>();
-        ticketPrintedTickets = new ArrayList<>();
-        jobInProgressTickets = new ArrayList<>();
-        newTickets = new ArrayList<>();
-        
-        mainFrame = new JFrame("NWNE Nexus Dashboard");
-        
-        layout = new GridBagLayout();
-        
-        mainFrame.setLayout(layout);
-        mainFrame.setPreferredSize(new Dimension(1024, 800));
-        
-        dataCollector = new DataCollector(issueReportedTickets, ticketPrintedTickets,
-                jobInProgressTickets, this, mysqlEngine);
-        
-        getData();
-        
-        dashBoardUpdate();
-        
-        mainFrame.pack();
-        mainFrame.setVisible(true);
-        
-    }
-    
-    private void getData()
-    {
-        dataCollector.start();
-    }
-    
-    public synchronized void dashBoardUpdate()
-    {
-        int i = TOPROW;
-        JPanel issueReportedHeader = new JPanel();
-        JLabel issueReportedHeaderLabel = new JLabel("<html><h1><b>Issue Reported</b></h1></html>");
-        GridBagConstraints issueReportedHeaderConstraints = template.
-                createGridBagConstraints(0, i, GridBagConstraints.NONE, 0, 0,
-                new Insets(0, 0, 0, 0), GridBagConstraints.CENTER, 0.0, 0.0, 1,
-                4);
-        i++;
-        Iterator<Ticket> it1 = issueReportedTickets.iterator();
-        while(it1.hasNext())
-        {
-            Ticket ticketToAdd = it1.next();
-            String textToDisplay = 
-                    "<html><h2><b> ID: </h2></b> <h4>" + ticketToAdd.getJobRefId() +
-                            "</h4><br><br>";
-            JLabel labelToAdd = new JLabel(textToDisplay);
-            JPanel panelToAdd = new JPanel();
-            panelToAdd.add(labelToAdd);
-            GridBagConstraints panelConstraints = template.
-                createGridBagConstraints(0, i, GridBagConstraints.NONE, 0, 0,
-                new Insets(0, 0, 0, 0), GridBagConstraints.CENTER, 0.0, 0.0, 1,
-                4);
-            i++;
-            mainFrame.add(panelToAdd, panelConstraints);
-        }
-        mainFrame.repaint();
-        System.out.println(issueReportedTickets);
     }
 
-    public synchronized void setIssueReportedTickets(ArrayList<Ticket> issueReportedTickets) {
-        this.issueReportedTickets = issueReportedTickets;
-    }
 
-    public synchronized void setTicketPrintedTickets(ArrayList<Ticket> ticketPrintedTickets) {
-        this.ticketPrintedTickets = ticketPrintedTickets;
-    }
 
-    public synchronized void setJobInProgressTickets(ArrayList<Ticket> jobInProgressTickets) {
-        this.jobInProgressTickets = jobInProgressTickets;
-    }
-
-    public synchronized void setNewTickets(ArrayList<Ticket> newTickets) {
-        this.newTickets = newTickets;
-    }
     
     
     
--- src/system/DataCollector.java
+++ src/system/DataCollector.java
@@ -4,92 +4,21 @@
  */
 package system;
 
-import gui.DashBoard;
-import io.MYSQLEngine;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.SwingWorker;
 
 /**
  *
  * @author Jonathan Rainer
  */
-public class DataCollector extends Thread {
-    
-    private ArrayList<Ticket> issueReportedTickets;
-    private ArrayList<Ticket> ticketPrintedTickets;
-    private ArrayList<Ticket> jobInProgressTickets;
-    private DashBoard dashBoard;
-    private MYSQLEngine mysqlEngine;
-    
-    public DataCollector(ArrayList<Ticket> issueReportedTickets,
-            ArrayList<Ticket> ticketPrintedTickets, ArrayList<Ticket>
-                    jobInProgressTickets, DashBoard dashBoard, 
-                    MYSQLEngine mysqlEngine)
-    {
-        this.issueReportedTickets = issueReportedTickets;
-        this.ticketPrintedTickets = ticketPrintedTickets;
-        this.jobInProgressTickets = jobInProgressTickets;
-        this.dashBoard = dashBoard;
-        this.mysqlEngine = mysqlEngine;
-    }
-    
+public class DataCollector extends SwingWorker
+{
+
     @Override
-    public synchronized void run()
+    protected Object doInBackground() throws Exception
     {
-        while(true)
-        {
-        ArrayList<Ticket> newIssueReportedTickets = mysqlEngine.
-                getIssueReportedTickets();
-        ArrayList<Ticket> newTicketPrintedTickets = mysqlEngine.
-                getTicketPrintedTickets();
-        ArrayList<Ticket> newJobInProgressTickets = mysqlEngine.
-                getjobInProgressTickets();
-        
-        int i = 0;
-        ArrayList<Ticket> newTickets = new ArrayList<>();
-        while (i < newIssueReportedTickets.size())
-        {
-            if(!issueReportedTickets.contains(newIssueReportedTickets.get(i)))
-            {
-                newTickets.add(newIssueReportedTickets.get(i));
-            }   
-            i++;
-        }
-        
-        int j = 0;
-        while (j < newTicketPrintedTickets.size())
-        {
-            if(!ticketPrintedTickets.contains(newTicketPrintedTickets.get(i)))
-            {
-                newTickets.add(newTicketPrintedTickets.get(i));
-            }   
-            j++;
-        }
-        
-        int k = 0;
-        while (k < newJobInProgressTickets.size())
-        {
-            if(!jobInProgressTickets.contains(newJobInProgressTickets.get(i)))
-            {
-                newTickets.add(newJobInProgressTickets.get(i));
-            }   
-            k++;
-        }
-        
-        dashBoard.setIssueReportedTickets(newIssueReportedTickets);
-        dashBoard.setTicketPrintedTickets(newTicketPrintedTickets);
-        dashBoard.setJobInProgressTickets(newJobInProgressTickets);
-        dashBoard.setNewTickets(newTickets);
-        
-        dashBoard.dashBoardUpdate();
-            try {
-                sleep(300000l);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(DataCollector.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
 }
 
  
